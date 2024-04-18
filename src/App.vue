154525437995c0c3bc8520bf1afc0ca98b16c22d
rrichardson <rrichardson@nowdigital.com.au>
2024-04-18 16:05:47 +1000

    Adding a footer (probably not needed)

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import Footer from "./components/Footer/index.vue"
     <FeatureHeroComponent />
     <FeatureCardItems />
   </main>
-
+  <Footer />
 </template>
 
 
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const currentYear = ref(new Date().getFullYear());
+</script>
+
+<template>
+  <footer class="bleed-bg">
+    <p>&copy;{{ currentYear }} Code Challenge Enterprises</p>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  text-align: center;
+  display: flex;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+  color: #fff;
+  background-color: var(--vt-c-black-mute);
+}
+
+.bleed-bg {
+  box-shadow: 0 100vmax 0 100vmax var(--vt-c-black-mute);
+  clip-path: inset(0 -100vmax 0 -100vmax);
+}
+</style>
